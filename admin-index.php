869b893cf5acbc07c37218acba905c3b2d3593c1
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2024-04-24 01:02:45 +0300

    вввв

---
+++ admin-index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adminpanel | Architecure</title>
+    <link rel="stylesheet" href="styles/style-admin.css">
+</head>
+
+<body>
+    <? require_once("./sections/header-admin.html")?>
+    <main>
+        <div class="container">
+            <h1>sladas</h1>
+        </div>
+    </main>
+</body>
+
+</html>
